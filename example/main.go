e0cb3d8f7d4c5d3fbb80a2967b44acbf3d2ad517
Valeriy Klyuchnikov <valeriy.v.klyuchnikov@gmail.com>
2023-10-22 12:22:20 +0300

    Fix import formatting

--- example/main.go
+++ example/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/KlyuchnikovV/webapi"
 	"github.com/KlyuchnikovV/webapi/example/services"
 	"github.com/KlyuchnikovV/webapi/response"
+	"log"
 )
 
 func main() {
