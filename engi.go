778d7e4406312bea956e88aa896b37085a035fd4
Valeriy Klyuchnikov <valeriy.v.klyuchnikov@gmail.com>
2024-04-18 13:29:27 +0300

    Preparation for documentation

--- engi.go
+++ engi.go
@@ -1,11 +1,9 @@
 package engi
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/KlyuchnikovV/engi/internal/types"
@@ -25,14 +23,12 @@ const (
 // Engine - server provider.
 type Engine struct {
 	apiPrefix string
-
-	server *http.Server
+	services  []*Service
 
 	responseMarshaler types.Marshaler
 	responseObject    types.Responser
 
-	services []*Service
-
+	server *http.Server
 	logger *slog.Logger
 }
 
@@ -59,51 +55,4 @@ func New(address string, configs ...Option) *Engine {
 	}
 
 	return engine
-}
-
-// RegisterServices - registering service routes.
-func (e *Engine) RegisterServices(services ...ServiceDefinition) error {
-	e.services = make([]*Service, len(services))
-
-	var mux = http.NewServeMux()
-
-	for i, service := range services {
-		var (
-			servicePath = fmt.Sprintf("%s/%s/", e.apiPrefix, service.Prefix())
-			srv         = NewService(e, service, servicePath)
-		)
-
-		e.services[i] = srv
-
-		for path, register := range service.Routers() {
-			register(
-				e.services[i],
-				strings.Trim(path, "/"),
-			)
-
-			e.services[i].logger.Debug("route registered",
-				slog.String("path", path),
-				slog.String("full_path", fmt.Sprintf("%s%s", servicePath, path)),
-			)
-		}
-
-		mux.Handle(servicePath, e.services[i])
-
-		e.logger.Debug("service registered", slog.String("service", service.Prefix()))
-	}
-
-	e.server.Handler = mux
-
-	return nil
-}
-
-// Start listens on the TCP network address srv.Addr and then calls Serve to handle requests on incoming connections.
-// Accepted connections are configured to enable TCP keep-alives.
-//
-// Start always returns a non-nil error. After Shutdown or Close, the returned error is ErrServerClosed.
-func (e *Engine) Start() error {
-	e.logger.Info("Starting engi", slog.String("address", e.server.Addr))
-	e.logger.Info("engi started...")
-
-	return e.server.ListenAndServe()
 }
